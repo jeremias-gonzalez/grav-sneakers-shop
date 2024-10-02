10d63e7ffdafde66477753ad0856d4b0930620ef
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2024-10-02 17:38:44 -0300

    main

--- src/assets/components/Logo/Logo.jsx
+++ src/assets/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from "../../../imgs/grav-high-resolution-logo-transparent.png"
 const Logo = () => {
   return (
-    <div><img className="h-8 w-30 ml-20 mx-auto md:h-10 md:w-20" src={logo} alt="logo" /></div>
+    <div><img className="h-8 w-30 ml-24 mx-auto md:h-10 md:w-20" src={logo} alt="logo" /></div>
   )
 }
 
